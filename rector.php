05af221c4c23f36ec6937f4bfb57b939b1962a45
Alexis <99094818+alexis-gss@users.noreply.github.com>
2026-06-08 09:01:26 +0000

    perf: 🚨 update php linters config

--- rector.php
+++ rector.php
@@ -1,16 +1,21 @@
 <?php
- 
+
 declare(strict_types=1);
- 
+
 use Rector\Config\RectorConfig;
+use RectorLaravel\Rector\Class_\CommandHiddenPropertyToHiddenAttributeRector;
+use RectorLaravel\Rector\Class_\DescriptionPropertyToDescriptionAttributeRector;
 use RectorLaravel\Rector\Class_\FillablePropertyToFillableAttributeRector;
 use RectorLaravel\Rector\Class_\HiddenPropertyToHiddenAttributeRector;
 use RectorLaravel\Rector\Class_\ModelCastsPropertyToCastsMethodRector;
+use RectorLaravel\Rector\Class_\SignaturePropertyToSignatureAttributeRector;
+use RectorLaravel\Rector\Class_\TablePropertyToTableAttributeRector;
+use RectorLaravel\Rector\Class_\WithoutTimestampsPropertyToWithoutTimestampsAttributeRector;
 use RectorLaravel\Rector\MethodCall\ValidationRuleArrayStringValueToArrayRector;
 use RectorLaravel\Set\LaravelLevelSetList;
 use RectorLaravel\Set\LaravelSetList;
 use RectorLaravel\Set\LaravelSetProvider;
- 
+
 return RectorConfig::configure()
     ->withSetProviders(LaravelSetProvider::class)
     ->withPaths([
@@ -20,7 +25,7 @@ return RectorConfig::configure()
     ->withPhpSets()
     ->withComposerBased(laravel: true)
     ->withSets([
-        LaravelLevelSetList::UP_TO_LARAVEL_120,
+        LaravelLevelSetList::UP_TO_LARAVEL_130,
         LaravelSetList::LARAVEL_ARRAYACCESS_TO_METHOD_CALL,
         LaravelSetList::LARAVEL_CODE_QUALITY,
         LaravelSetList::LARAVEL_COLLECTION,
@@ -32,8 +37,16 @@ return RectorConfig::configure()
     ->withDeadCodeLevel(0)
     ->withCodeQualityLevel(0)
     ->withSkip([
+        // Disable Laravel notations.
+        CommandHiddenPropertyToHiddenAttributeRector::class,
+        DescriptionPropertyToDescriptionAttributeRector::class,
         FillablePropertyToFillableAttributeRector::class,
         HiddenPropertyToHiddenAttributeRector::class,
         ModelCastsPropertyToCastsMethodRector::class,
+        SignaturePropertyToSignatureAttributeRector::class,
+        TablePropertyToTableAttributeRector::class,
+        WithoutTimestampsPropertyToWithoutTimestampsAttributeRector::class,
+
+        // Validation.
         ValidationRuleArrayStringValueToArrayRector::class,
     ]);
